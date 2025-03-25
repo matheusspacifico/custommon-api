ece39cf1b03ad3f899a770461480221a5fb890c8
matheusspacifico <matheuspacifico44@gmail.com>
2025-03-25 01:23:25 -0300

    feat(type): type enum now has weaknesses

--- src/main/kotlin/com/custommon/Type.kt
+++ src/main/kotlin/com/custommon/Type.kt
@@ -18,5 +18,34 @@ enum class Type {
     DRAGON,
     DARK,
     STEEL,
-    FAIRY
+    FAIRY;
+
+    lateinit var weaknesses: List<Type>
+
+    companion object {
+        fun initializeWeaknesses() {
+            NORMAL.weaknesses = listOf(FIGHTING)
+            FIRE.weaknesses = listOf(WATER, GROUND, ROCK)
+            WATER.weaknesses = listOf(ELECTRIC, GRASS)
+            GRASS.weaknesses = listOf(FIRE, ICE, POISON, FLYING, BUG)
+            ELECTRIC.weaknesses = listOf(GROUND)
+            ICE.weaknesses = listOf(FIRE, FIGHTING, ROCK, STEEL)
+            FIGHTING.weaknesses = listOf(FLYING, PSYCHIC, FAIRY)
+            POISON.weaknesses = listOf(GROUND, PSYCHIC)
+            GROUND.weaknesses = listOf(WATER, GRASS, ICE)
+            FLYING.weaknesses = listOf(ELECTRIC, ROCK, ICE)
+            PSYCHIC.weaknesses = listOf(BUG, GHOST, DARK)
+            BUG.weaknesses = listOf(FIRE, FLYING, ROCK)
+            ROCK.weaknesses = listOf(WATER, GRASS, FIGHTING, GROUND, STEEL)
+            GHOST.weaknesses = listOf(GHOST, DARK)
+            DRAGON.weaknesses = listOf(ICE, DRAGON, FAIRY)
+            DARK.weaknesses = listOf(FIGHTING, BUG, FAIRY)
+            STEEL.weaknesses = listOf(FIRE, FIGHTING, GROUND)
+            FAIRY.weaknesses = listOf(POISON, STEEL)
+        }
+
+        fun getWeaknesses(types: List<Type>): List<Type> {
+            return types.flatMap { it.weaknesses }.distinct()
+        }
+    }
 }
