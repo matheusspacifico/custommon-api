afd02ee0507c377ac88e70124f31e2e385ffff43
matheusspacifico <matheuspacifico44@gmail.com>
2025-03-25 00:16:31 -0300

    refactor: removed unused imports

--- src/main/kotlin/com/custommon/CustommonRepository.kt
+++ src/main/kotlin/com/custommon/CustommonRepository.kt
@@ -1,9 +1,7 @@
 package com.custommon
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
 interface CustommonRepository : JpaRepository<Custommon, Int>
