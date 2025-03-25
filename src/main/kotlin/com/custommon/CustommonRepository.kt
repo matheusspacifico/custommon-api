be708aabe2b9e3c3eba718b6220dcefa5ac6a378
matheusspacifico <matheuspacifico44@gmail.com>
2025-03-25 00:14:16 -0300

    refactor: repository interface

--- src/main/kotlin/com/custommon/CustommonRepository.kt
+++ src/main/kotlin/com/custommon/CustommonRepository.kt
@@ -1,8 +1,9 @@
 package com.custommon
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface CustommonRepository : JpaRepository<Custommon, Int> {
-}
+interface CustommonRepository : JpaRepository<Custommon, Int>
